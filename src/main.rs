7cfd36210dd23359cb74da58ffb81afea194ecd6
luojia65 <me@luojia.cc>
2020-04-24 22:15:26 +0800

    Update K210 example to use 0.2 version of HAL

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,28 @@
 #![no_std]
 #![no_main]
 
-extern crate panic_halt;
-
-use riscv_rt::entry;
+use panic_halt as _;
 use k210_hal::prelude::*;
 use k210_hal::pac as pac;
 use k210_hal::stdout::Stdout;
 
-#[entry]
+#[riscv_rt::entry]
 fn main() -> ! {
-    let p = pac::Peripherals::take().unwrap();
+    let p = k210_hal::Peripherals::take().unwrap();
+
+    // Prepare pins for UARTHS
+    let tx = p.pins.pin5;
+    let rx = p.pins.pin4;
 
     // Configure clocks (TODO)
     let clocks = k210_hal::clock::Clocks::new();
 
     // Configure UART
-    let serial = p.UARTHS.configure(115_200.bps(), &clocks);
+    let serial = p.UARTHS.configure(
+        (tx, rx),
+        115_200.bps(), 
+        &clocks
+    );
     let (mut tx, _) = serial.split();
 
     let mut stdout = Stdout(&mut tx);
