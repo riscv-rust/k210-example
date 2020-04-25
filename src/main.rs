20bf85964818049e7640fe3a90753bf69a53406a
luojia65 <me@luojia.cc>
2020-04-25 15:23:53 +0800

    Update example for new FPIOA adapter design

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,36 @@
 
 use panic_halt as _;
 use k210_hal::prelude::*;
+use k210_hal::fpioa;
 use k210_hal::pac as pac;
 use k210_hal::stdout::Stdout;
 
 #[riscv_rt::entry]
 fn main() -> ! {
-    let p = k210_hal::Peripherals::take().unwrap();
+    let p = pac::Peripherals::take().unwrap();
 
     // Prepare pins for UARTHS
-    let tx = p.pins.pin5;
-    let rx = p.pins.pin4;
+    let fpioa = p.FPIOA.split();
+    // let tx = p.pins.pin5;
+    let _io5 = fpioa.io5.into_function(fpioa::UarthsTx);
+    // let rx = p.pins.pin4;
 
     // Configure clocks (TODO)
     let clocks = k210_hal::clock::Clocks::new();
 
     // Configure UART
     let serial = p.UARTHS.configure(
-        (tx, rx),
         115_200.bps(), 
         &clocks
     );
     let (mut tx, _) = serial.split();
 
+    // todo: new stdout design (simple Write impl?)
     let mut stdout = Stdout(&mut tx);
 
     writeln!(stdout, "Hello, Rust!").unwrap();
 
     loop {
-        writeln!(stdout, "Hello again!").unwrap();
+        writeln!(stdout, "Rust NB!").unwrap();
     }
 }
