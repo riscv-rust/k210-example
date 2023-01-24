6cfe3a2000752d55772ef34c664ddabe6ff7e578
Alexis Paques <alexis.paques@gmail.com>
2023-01-24 19:57:16 +0100

    Add the built-in RGB leds in the example

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -13,18 +13,33 @@ fn main() -> ! {
     let mut sysctl = p.SYSCTL.constrain();
     let fpioa = p.FPIOA.split(&mut sysctl.apb0);
     let gpio = p.GPIO.split(&mut sysctl.apb0);
+    let io12 = fpioa.io12.into_function(fpioa::GPIO7);
+    let io13 = fpioa.io13.into_function(fpioa::GPIO5);
     let io14 = fpioa.io14.into_function(fpioa::GPIO6);
+    let mut green = Gpio::new(gpio.gpio7, io12).into_push_pull_output();
+    let mut red = Gpio::new(gpio.gpio5, io13).into_push_pull_output();
     let mut blue = Gpio::new(gpio.gpio6, io14).into_push_pull_output();
 
-    blue.set_low().unwrap();
+    red.set_high().unwrap();
+    green.set_high().unwrap();
+    blue.set_high().unwrap();
 
     let mut last_update = riscv::register::mcycle::read();
+    let mut i = 0;
     loop {
         let cur = riscv::register::mcycle::read();
         if cur - last_update >= 100_000_000 {
             last_update = cur;
 
-            blue.toggle().unwrap();
+            red.toggle().unwrap();
+            if i % 2 == 0 {
+              green.toggle().unwrap();
+            }
+            if i % 3 == 0 {
+              blue.toggle().unwrap();
+            }
+
+            i += 1;
         }
     }
 }
